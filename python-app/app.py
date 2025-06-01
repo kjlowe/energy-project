79a1ff4f72bbbcb888594a771d5518d35277cc9f
Kevin Lowe <k.james.lowe@gmail.com>
2025-05-31 20:47:30 -0700

    Update app.py again

--- python-app/app.py
+++ python-app/app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Python! Did it work?"
+    return "Hello from Python! Did it work now?"
 
 app.run(host="0.0.0.0", port=5000)
