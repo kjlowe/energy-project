2fc0b71aaece1a034c9b64f30b6fbaaf1db83945
Kevin Lowe <k.james.lowe@gmail.com>
2025-06-12 22:45:28 -0700

    Update app.py to show the deploy is working

--- python-app/app.py
+++ python-app/app.py
@@ -8,6 +8,6 @@ def home():
     # calculate today's date
     # and return a greeting message
     today = datetime.now().strftime("%Y-%m-%d")
-    return f"Today's date is: {today} and I'm ready for it!"
+    return f"Today's date is: {today} and I'm ready for it! And it's up to date!"
 
 app.run(debug=True, host="0.0.0.0", port=5000)
